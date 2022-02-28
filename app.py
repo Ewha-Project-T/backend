b82ef1989bd1ad9f7100f39d9ce5ab22a06e4649
goseungduk <a457581@catholic.ac.kr>
2022-03-01 04:37:31 +0900

    rewind

--- app.py
+++ app.py
@@ -1,17 +1,11 @@
 from flask import Flask
-from flask_restful import Api
-from flasgger import Swagger
-from flask_cors import CORS
-from server.apis import load_api
 
 app=Flask(__name__)
-CORS(app) # 모든 출처에 대해서 CORS 허용 세세한 설정 추후 논의
-myApi=Api(app)
-swagger = Swagger(app)
 
 @app.route("/", methods=['GET'])
 def hello():
-    return "CAT-Secuirty"
+    a = 0
+    return "Hello"
 
-load_api(myApi)
-app.run(host='0.0.0.0', port = 5000)
+if __name__=="__main__":
+    app.run(host='0.0.0.0', port = 5000)

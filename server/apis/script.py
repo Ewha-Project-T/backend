2a2b5120bb820870b0691c4dba50986861b3859a
goseungduk <a457581@catholic.ac.kr>
2022-03-09 17:18:32 +0900

    feat: script upload/down

--- server/apis/script.py
+++ server/apis/script.py
@@ -1,6 +1,7 @@
 from flask import jsonify, make_response
 from flask_restful import reqparse, Resource
 from flask_jwt_extended import jwt_required
+import werkzeug
 
 class ScriptAPI(Resource):
     @jwt_required()
@@ -13,9 +14,18 @@ class ScriptAPI(Resource):
         parser.add_argument('file_name', type=str, required=True, help="FileName is required")
         args = parser.parse_args()
         filename = args['file_name']
-        print(filename)
-        response = make_response(filename)
-        response.headers['Content-Type'] = 'text/json'
-        response.headers['Content-Disposition'] = 'attachment; filename=selected_items.json'
-        return response, 200
+        headers = {}
+        headers['Content-Type'] = 'text/json'
+        headers['Content-Disposition'] = 'attachment; filename=selected_items.json'
+        return {"msg": filename}, 200, headers
+
+    @jwt_required()
+    def put(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('file_name', type=werkzeug.datastructures.FileStorage, location='files')
+        args = parser.parse_args()
+        file_object = args['file_name']
+        print(file_object)
+        file_object.save("test.txt")
+        return {"msg":"good"}, 200
 

2a9ababb18485d55e2fdbf572091bf08d045252c
goseungduk <a457581@catholic.ac.kr>
2022-03-09 16:43:17 +0900

    fix: scan api renaming

--- server/apis/analysis.py
+++ server/apis/analysis.py
@@ -2,7 +2,7 @@ from flask import jsonify
 from flask_restful import reqparse, Resource
 from flask_jwt_extended import jwt_required
 
-class Scan(Resource):
+class Analysis(Resource):
     @jwt_required()
     def get(self):
         return {"msg":"scanAPI"}, 200

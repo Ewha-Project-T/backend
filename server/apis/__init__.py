32c6306971b50c93cce04860a16ae96e72839771
goseungduk <a457581@catholic.ac.kr>
2022-03-09 18:52:46 +0900

    feat: Script file upload/download API finish

--- server/apis/__init__.py
+++ server/apis/__init__.py
@@ -9,5 +9,6 @@ def load_api(api_module):
     api_module.add_resource(Login,'/login',endpoint='login')
     api_module.add_resource(LoginRefresh,'/login-refresh',endpoint='login_refresh')
     api_module.add_resource(Account,'/del-account',endpoint='del_account')
+    api_module.add_resource(ScriptAPI,'/script/<fname>',endpoint='script_get')
     api_module.add_resource(ScriptAPI,'/script',endpoint='script')
     api_module.add_resource(Analysis,'/analysis',endpoint='analysis')

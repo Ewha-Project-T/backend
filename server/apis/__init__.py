0d155c3b0957d666e1ab92b2cfa646e9561ae369
twOn3 <tw0n3pwn@gmail.com>
2022-03-15 00:15:09 +0900

    fix: Password Encrypt & XML Parser modify

--- server/apis/__init__.py
+++ server/apis/__init__.py
@@ -1,7 +1,7 @@
 from .login import Login, LoginRefresh, Account
 from .script import ScriptAPI
 from .analysis import Analysis
-from .result import Result
+from .parser import XML_Parser
 
 def load_api(api_module):
     api_module.add_resource(Login,'/login',endpoint='login') # GET POST PUT PATCH DELETE
@@ -10,4 +10,4 @@ def load_api(api_module):
     api_module.add_resource(ScriptAPI,'/script/<fname>',endpoint='script_get') # GET - DOWNLOAD
     api_module.add_resource(ScriptAPI,'/script',endpoint='script') # POST - UPLOAD
     api_module.add_resource(Analysis,'/analysis',endpoint='analysis')
-    api_module.add_resource(Result,'/result',endpoint='result')
+    api_module.add_resource(XML_Parser,'/parser',endpoint='parser')
--- server/apis/parser.py
+++ server/apis/parser.py
@@ -4,17 +4,17 @@ from flask_jwt_extended import (
 		jwt_required, get_jwt_identity, get_jwt
 )
 import re
-from ..services.xml_parser import xml_result, ParseResult
+from ..services.xml_parser import parse_xml, ParseResult
 
-class Result(Resource):
-    @swag_from("../../docs/result/parser.yml")
+class XML_Parser(Resource):
+    @swag_from("../../docs/parser/post.yml")
     #@jwt_required()
     def post(self):
         parser = reqparse.RequestParser()
-        parser.add_argument('file_name', type=str, required=True, help="File Not Found")
+        parser.add_argument('xml_name', type=str, required=True, help="File Not Found")
         args = parser.parse_args()
-        file_name = args['file_name']
-        result, group_code, group_name, title_code, title_name, important, decision, issue = xml_result(file_name)
+        xml_name = args['xml_name']
+        result, group_code, group_name, title_code, title_name, important, decision, issue = parse_xml(xml_name)
         if(result==ParseResult.SUCCESS):
             #return group_code, group_name, title_code, title_name, important, decision, issue
             return {"msg":"success"}, 200

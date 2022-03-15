72c679271bfe1a70a3c3cca56bf190459c1a50b6
wsoh9812 <wsoh9812@naver.com>
2022-03-15 00:13:03 -0700

    a

--- server/apis/__init__.py
+++ server/apis/__init__.py
@@ -9,7 +9,7 @@ def load_api(api_module):
     api_module.add_resource(Account,'/del-account',endpoint='del_account') # DELETE
     api_module.add_resource(ScriptAPI,'/script/<fname>',endpoint='script_get') # GET - DOWNLOAD
     api_module.add_resource(ScriptAPI,'/script',endpoint='script') # POST - UPLOAD
-    api_module.add_resource(Analysis,'/script/<filename>') # download
+    api_module.add_resource(Analysis,'/analysis/<filename>') # download
     api_module.add_resource(Analysis,'/analysis',endpoint='analysis')
     api_module.add_resource(XML_Parser,'/parser',endpoint='parser')
     api_module.add_resource(ScriptListingAPI,'/script-list',endpoint='script_list')

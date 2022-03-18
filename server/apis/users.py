068cc64d02145ab8b3909cacd6e79a0f66014632
goseungduk <a457581@catholic.ac.kr>
2022-03-18 19:42:11 +0900

    feat: ADMIN API - Initializing User Login Fail Count

--- server/apis/users.py
+++ server/apis/users.py
@@ -7,7 +7,7 @@ from ..services.login_service import (
     admin_required, DeleteResult
 )
 from ..services.admin_service import (
-    get_users_info, delete_user
+    get_users_info, delete_user, init_user_limit, InitResult
 )
 from server import db
 from ..model import User
@@ -17,6 +17,19 @@ class Users(Resource):
     def get(self):
         res=get_users_info()
         return jsonify(users_info=res)
+        
+    @admin_required()
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('user_no', type=str, required=True, help="user_no is required")
+        args = parser.parse_args()
+        user_no = args['user_no']
+        result = init_user_limit(user_no)
+        if(result == InitResult.SUCCESS):
+            return {"msg":"Init fail limit success"}, 200
+        else:
+            return {"msg":"INVALID USER No"}, 403
+
     @admin_required()
     def delete(self,user_no):
         result = delete_user(user_no)
--- server/services/admin_service.py
+++ server/services/admin_service.py
@@ -2,6 +2,10 @@ from .login_service import DeleteResult
 from ..model import User
 from server import db
 
+class InitResult:
+    SUCCESS = 0
+    INVALID_USER_NO = 1
+
 def get_users_info():
     acc_list = User.query.all()
     info_list = []
@@ -16,6 +20,15 @@ def get_users_info():
         tmp_info["login_fail"] = info.login_fail_limit
         info_list.append(tmp_info)
     return info_list
+
+def init_user_limit(user_no):
+    acc = User.query.filter_by(user_no=user_no).first()
+    if(acc==None):
+        return InitResult.INVALID_USER_NO
+    acc.login_fail_limit = 0
+    db.session.add(acc)
+    db.session.commit()
+    return InitResult.SUCCESS
 
 def delete_user(user_no):
     acc = User.query.filter_by(user_no=user_no).first()

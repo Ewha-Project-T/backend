2683c29727eb5de2482fdc93985bd744420bc2c9
goseungduk <a457581@catholic.ac.kr>
2022-03-20 23:46:40 +0900

    feat: admin api - all script listing

--- server/apis/__init__.py
+++ server/apis/__init__.py
@@ -1,5 +1,5 @@
 from .login import Login, LoginRefresh, Account, Admin
-from .script import ScriptAPI, ScriptListingAPI
+from .script import ScriptAPI, ScriptListingAPI, AdminScript
 from .analysis import Analysis
 from .project import Project, ProjectList
 from .parser import XML_Parser
@@ -12,12 +12,13 @@ def load_api(api_module):
     api_module.add_resource(Account,'/del-account',endpoint='del_account') # DELETE
     api_module.add_resource(ScriptAPI,'/script/<fname>',endpoint='script_get') # GET - DOWNLOAD
     api_module.add_resource(ScriptAPI,'/script',endpoint='script') # POST - UPLOAD
+    api_module.add_resource(ScriptListingAPI,'/script-list',endpoint='script_list')
+    api_module.add_resource(AdminScript,'/scripts',endpoint='admin_scripts')
     api_module.add_resource(Analysis,'/analysis',endpoint='analysis')
     api_module.add_resource(Project,'/project',endpoint='project')
     api_module.add_resource(Project,'/project/<project_no>',endpoint='project_del-patch')
     api_module.add_resource(ProjectList, '/project-list', endpoint='project_list')
     api_module.add_resource(XML_Parser,'/parser',endpoint='parser')
-    api_module.add_resource(ScriptListingAPI,'/script-list',endpoint='script_list')
     api_module.add_resource(Users,'/users',endpoint='users')
     api_module.add_resource(Users,'/users/<user_no>',endpoint='users_del')
 

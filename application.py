083c9ba3dca554edf955d2545f3a714f3564d87f
goseungduk <a457581@catholic.ac.kr>
2022-04-07 14:56:50 +0900

    feat: Refresh token expiring in one day

--- application.py
+++ application.py
@@ -17,6 +17,7 @@ app.config['SWAGGER'] = {
 
 app.config['SECRET_KEY'] = 'CAT-Security-King-God'
 app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
+app.config["JWT_REFRESH_TOKEN_EXPIRES"] = timedelta(days=1)
 app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://cert:cert@203.229.206.16:12344/cert"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True 

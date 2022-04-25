086ca8e3944d63ce248e8100bb36fc81467dc1d8
goseungduk <a457581@catholic.ac.kr>
2022-04-25 14:54:44 +0900

    fix: AdminAPI - Patch user error handling

--- server/services/admin_service.py
+++ server/services/admin_service.py
@@ -79,28 +79,29 @@ def all_project_script_listing(project_no):
         script_list_result.append(tmp)
     return script_list_result
 
-    
-def patch_user(user_no,new_id,new_pw,email,name):
+def patch_user(user_no, new_id, new_pw, email, name):
     acc = User.query.filter_by(user_no=user_no).first()
     if(new_id != None):
         new_id_check = User.query.filter_by(id=new_id).first()
         if(new_id_check != None):
-            return UserChangeResult.DUPLICATED_ID
+            if(new_id_check.user_no != user_no):
+                return UserChangeResult.DUPLICATED_ID
         acc.id = new_id
     if(email != None):
         email_check = User.query.filter_by(email=email).first()
         if(email_check != None):
-            return UserChangeResult.DUPLICATED_EMAIL
+            if(email_check.user_no != user_no):
+                return UserChangeResult.DUPLICATED_EMAIL
         acc.email = email
     if(acc == None):
         return UserChangeResult.INVALID_USER
+    if(name != None):
+        acc.name = name
     if(new_pw!=None):
         password = base64.b64decode(acc.password)
         salt = password[:32]
         new_pw = base64.b64encode(salt + hashlib.pbkdf2_hmac('sha256', new_pw.encode('utf-8'), salt, 100000, dklen=128))
         acc.password = new_pw
-    if(name != None):
-        acc.name = name
     db.session.add(acc)
     db.session.commit()
     return UserChangeResult.SUCCESS

0ea7b1b19502d02fcd0ef4275e4074c299a28739
goseungduk <a457581@catholic.ac.kr>
2022-05-05 02:14:04 +0900

    feat: run_server script add

---
+++ run_server.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+XML_DIR = "uploads"
+SCRIPT_DIR = "script_files"
+if [ ! -e $XML_DIR ]; then
+        mkdir uploads
+fi
+if [ ! -e $SCRIPT_DIR ]; then
+        mkdir script_files
+fi
+nohup python3 -u ./application.py &

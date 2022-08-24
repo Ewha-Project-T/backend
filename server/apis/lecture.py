8c767ae96bbbc5d759e2da7371f05590b6fef35e
gud425 <gud425@naver.com>
2022-08-24 09:57:47 +0900

    강의수정

--- server/apis/lecture.py
+++ server/apis/lecture.py
@@ -42,27 +42,6 @@ class Lecture_mod_del(Resource):
         else:
             return{"msg": "access denied"}
 
-    def post(self):#강의수정권한관리 만든사람, 관리자
-        parser = reqparse.RequestParser()
-        parser.add_argument('lecture_no', type=int, required=True, help="lecture_no is required")
-        parser.add_argument('name', type=str, required=True, help="Nameis required")
-        parser.add_argument('year', type=str, required=True, help="Year is required")
-        parser.add_argument('semester', type=str, required=True, help="Semester is required")
-        parser.add_argument('major', type=str, required=True, help="Major is required")
-        parser.add_argument('separated', type=str, required=True, help="separated id is required")
-        parser.add_argument('professor', type=str, required=True, help="professor is required")
-
-        args = parser.parse_args()
-        lecture_no = args['lecture_no']
-        lecture_name = args['name']
-        lecture_year = args['year']
-        lecture_semester = args['semester']
-        lecture_major=args['major']
-        lecture_separated= args['separated']
-        lecture_professor = args['professor']
-        modify_lecture(lecture_no,lecture_name,lecture_year,lecture_semester,lecture_major,lecture_separated,lecture_professor)#추후 에러코드관리
-        return{"msg" : "lecture modify success"},200
-
 class Student(Resource):
     def get(self):#학생조회 이름과 전공으로 검색 후 리스팅
         parser = reqparse.RequestParser()
@@ -136,3 +115,38 @@ class Lecture_add(Resource):
         make_lecture(lecture_name,lecture_year,lecture_semester,lecture_major,lecture_separated,lecture_professor,attendee,user_info)#추후 에러코드관리
         return{"msg" : "lecture make success"},201
 
+class Lecture_mod(Resource):
+    @jwt_required()
+    def get(self):
+        user_list=get_all_user()
+        parser = reqparse.RequestParser()
+        parser.add_argument('lecture_no', type=int )
+        args = parser.parse_args()
+        lecture_no = args['lecture_no']
+        return make_response(render_template("lecture_mod.html",user_list=user_list,lecture_no=lecture_no))
+    @jwt_required()
+    def post(self):#강의수정권한관리 만든사람, 관리자
+        user_info=get_jwt_identity()
+        parser = reqparse.RequestParser()
+        parser.add_argument('lecture_no', type=int )
+        parser.add_argument('lecture_name', type=str )
+        parser.add_argument('lecture_year', type=str )
+        parser.add_argument('lecture_semester', type=str)
+        parser.add_argument('lecture_major', type=str)
+        parser.add_argument('lecture_separated', type=str)
+        parser.add_argument('lecture_professor', type=str)
+        parser.add_argument('lecture_attendee', type=str, action='append')
+        args = parser.parse_args()
+        lecture_no = args['lecture_no']
+        lecture_name = args['lecture_name']
+        lecture_year = args['lecture_year']
+        lecture_semester = args['lecture_semester']
+        lecture_major=args['lecture_major']
+        lecture_separated= args['lecture_separated']
+        lecture_professor = args['lecture_professor']
+        attendee=args['lecture_attendee']
+        if(lecture_access_check(user_info["user_no"],lecture_no) or user_info["user_perm"]==0):
+            modify_lecture(lecture_no,lecture_name,lecture_year,lecture_semester,lecture_major,lecture_separated,lecture_professor,attendee,user_info)#추후 에러코드관리
+            return{"msg" : "lecture modify success"},200
+        else:
+            return{"msg": "access denied"}

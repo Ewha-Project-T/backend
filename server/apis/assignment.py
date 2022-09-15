75e5069e9a5eb029d8747a544fd3bc6336c7a099
gud425 <gud425@naver.com>
2022-09-15 14:45:22 +0900

    fix:feedback

--- server/apis/assignment.py
+++ server/apis/assignment.py
@@ -207,7 +207,7 @@ class Prob_feedback(Resource):
             return redirect(host_url + url_for('prob', lecture_no=lecture_no))
         original_stt_result=get_original_stt_result(wav_url_example)
         as_info=get_as_info(lecture_no,as_no)
-        return make_response(render_template("prob_feedback.html",user_info=user_info,as_name=as_name,wav_url=wav_url,wav_url_example=wav_url_example,stt_result=stt_result,original_stt_result=original_stt_result,as_info=as_info))
+        return make_response(render_template("prob_feedback.html",user_info=user_info,as_name=as_name,wav_url=wav_url,wav_url_example=wav_url_example,stt_result=stt_result,original_stt_result=original_stt_result,as_info=as_info,lecture_no=lecture_no,as_no=as_no))
 
 
     @jwt_required()

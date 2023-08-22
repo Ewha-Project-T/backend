32357f874099e9c211ed164cc340e46da5fa6c29
맥프로2222 <maegpeulo2222@nlp.ewha.ac.kr>
2023-08-22 16:35:21 +0900

    Feat: 테스트용

--- server/apis/__init__.py
+++ server/apis/__init__.py
@@ -1,16 +1,37 @@
 from server.apis.mail import Email, Verify_email
 from server.model import Lecture
-from .login import Login,Join, LoginRefresh, Logout,Login2,Logout2,CheckToken
+from .login import Login,Join, LoginRefresh, Logout
 from .lecture import Attend, Lecture, Lecture_mod, Lecture_mod_del, Student,Major, Lecture_add
 from .assignment import Prob, Prob_del, Prob_submit, Prob_feedback,Prob_mod,Prob_add, prob_upload, Prob_submit_list
 from .admin import Admin,Admin2
 from .stt import Stt, SttJob, SttSeq, SttSeqJob
+from .react_login import Login2,Logout2,CheckToken,Join2
+from .react_assignment import Feedback, Feedback2, React_Prob, React_Prob_del,React_Prob_add,React_Prob_mod,React_Prob_submit_list,React_Prob_submit,Studentgraphlist,Professorgraphlist
+from .react_lecture import React_Lecture,React_Lecture_mod_del,React_Student,React_Lecture_add,React_Lecture_mod
+
+
 
 def load_api(api_module):
     api_module.add_resource(Login,'/login',endpoint='login') # GET POST PUT PATCH DELETE
     api_module.add_resource(Login2,'/api/user/login',endpoint='login2') # GET POST PUT PATCH DELETE
     api_module.add_resource(Logout2,'/api/user/logout',endpoint='logout2') # GET POST PUT PATCH DELETE
+    api_module.add_resource(Join2,'/api/user/join',endpoint='join2') # GET POST PUT PATCH DELETE
     api_module.add_resource(CheckToken,'/api/user/auth',endpoint='checkToken') 
+    api_module.add_resource(React_Lecture,'/api/lecture/list',endpoint='react_lecture_list')
+    api_module.add_resource(React_Lecture_mod_del,'/api/lecture/delete',endpoint='react_lecture_delete')
+    api_module.add_resource(React_Student,'/api/lecture/studentlist',endpoint='react_student')
+    api_module.add_resource(React_Lecture_add,'/api/lecture/create',endpoint='react_lecture_add')
+    api_module.add_resource(React_Lecture_mod,'/api/lecture/modify',endpoint='react_lecture_mod')
+    api_module.add_resource(React_Prob, '/api/prob/list', endpoint='react_prob')
+    api_module.add_resource(React_Prob_del, '/api/prob/delete', endpoint='react_prob_del')
+    api_module.add_resource(React_Prob_add, '/api/prob/create', endpoint='react_prob_add')
+    api_module.add_resource(React_Prob_mod, '/api/prob/modify', endpoint='react_prob_mod')
+    api_module.add_resource(React_Prob_submit_list,'/api/probsubmit/list',endpoint='react_prob_submit_list')
+    api_module.add_resource(React_Prob_submit, '/api/prob/submit', endpoint='react_prob_submit')
+    api_module.add_resource(Studentgraphlist,'/api/feedback/studentgraphlist',endpoint='studentGraphlist')
+    api_module.add_resource(Professorgraphlist,'/api/feedback/professorgraphlist',endpoint='professorgraphlist')
+    api_module.add_resource(Feedback,'/r_feedback',endpoint='r_feedback')
+    api_module.add_resource(Feedback2,'/test',endpoint='test')
     api_module.add_resource(LoginRefresh,'/login-refresh',endpoint='login_refresh') 
     api_module.add_resource(Lecture,'/lecture',endpoint='lecture')
     api_module.add_resource(Admin,'/admin',endpoint='admin')
---
+++ server/apis/react_lecture.py
@@ -0,0 +1,175 @@
+import json
+from operator import le
+from datetime import datetime
+from pickle import TRUE
+from flask import jsonify,render_template, request, redirect, url_for,abort,make_response
+from flask_restful import reqparse, Resource
+from flask_jwt_extended import (
+    jwt_required, get_jwt_identity, create_access_token, get_jwt
+)
+import re
+from ..services.lecture_service import lecture_listing, make_lecture,modify_lecture,delete_lecture, search_student,major_listing,attendee_add,attendee_listing,lecture_access_check,mod_lecutre_listing
+from ..services.login_service import (
+     admin_required, professor_required, assistant_required,get_all_user
+)
+from os import environ as env
+host_url=env["HOST"]
+perm_list={"학생":1,"조교":2,"교수":3}
+class React_Lecture(Resource):
+    @jwt_required()
+    def get(self):
+        user_info=get_jwt_identity()
+        if(user_info["user_perm"]==0):
+            lecture_list=lecture_listing()
+        else:
+            lecture_list=lecture_listing(user_info)
+        
+        return {   "lecturelist":lecture_list,
+                       "userInfo": user_info,
+                       "role": user_info["user_perm"],
+                       },200
+
+
+
+class React_Lecture_mod_del(Resource):
+    
+    @professor_required()
+    def get(self):#강의삭제/권한관리 만든사람, 관리자
+        parser = reqparse.RequestParser()
+        parser.add_argument('lecture_no', type=int)
+        args = parser.parse_args()
+        lecture_no = args['lecture_no']
+        user_info=get_jwt_identity()
+        if(lecture_access_check(user_info["user_no"],lecture_no) or user_info["user_perm"]==0):
+            delete_lecture(lecture_no)
+            return{"msg" : "lecture delete success"},200
+        else:
+            return{"msg": "access denied"}
+
+class React_Student(Resource):
+    @professor_required()
+    def get(self):#학생조회 이름과 전공으로 검색 후 리스팅
+        parser = reqparse.RequestParser()
+        parser.add_argument('name', type=str)
+        parser.add_argument('major', type=str)
+        args = parser.parse_args()
+        user_name=args['name']
+        user_major= args['major']
+        user_list = search_student(user_name,user_major)
+        return {"userlist":user_list},200
+
+        
+    
+class Major(Resource):
+    @professor_required()
+    def get(self):#해당전공 과목 리스팅
+        parser=reqparse.RequestParser()
+        parser.add_argument('major',type=str)
+        args=parser.parse_args()
+        user_major=args['major']
+        major_list= major_listing(user_major)
+        return jsonify(major_list=major_list)
+
+class Attend(Resource):
+    @professor_required()
+    def get(self):# lecture no로 해당 강의의 수강생 명단 리스팅
+        parser=reqparse.RequestParser()
+        parser.add_argument('lecture_no',type=int)
+        args=parser.parse_args()
+        lecture_no=args['lecture_no']
+        attendee_list=attendee_listing(lecture_no)
+        return jsonify(attendee_list=attendee_list)
+    @professor_required()
+    def post(self):#수강생 명단추가
+        parser=reqparse.RequestParser()
+        parser.add_argument('user_no',type=int)
+        parser.add_argument('lecture_no',type=int)
+        parser.add_argument('permission',type=str)
+        args=parser.parse_args()
+        user_no=args['user_no']
+        lecture_no=args['lecture_no']
+        permission=args['permission']
+        if args['permission'] not in perm_list:
+            permission=1
+        else:
+            permission=perm_list[args['permission']]
+        attendee_add(user_no,lecture_no,permission)
+        return {"msg":"add success"}
+
+class React_Lecture_add(Resource):
+    @professor_required()
+    def get(self):
+        user_list=get_all_user()
+        user_info=get_jwt_identity()
+        now = datetime.now()
+        return {   "userlist":user_list,
+                       "userInfo": user_info,
+                       
+                       },200
+    
+    @professor_required()
+    def post(self):#강의생성/교수이상의권한
+        user_info=get_jwt_identity()
+        parser = reqparse.RequestParser()
+        parser.add_argument('lecture_name', type=str, required=True, help="Nameis required")
+        parser.add_argument('lecture_year', type=str, required=True, help="Year is required")
+        parser.add_argument('lecture_semester', type=str, required=True, help="Semester is required")
+        parser.add_argument('lecture_major', type=str, required=True, help="Major is required")
+        parser.add_argument('lecture_separated', type=str, required=True, help="separated id is required")
+        parser.add_argument('lecture_professor', type=str, required=True, help="professor is required")
+        parser.add_argument('lecture_attendee', type=str, action='append', required=True)
+        args = parser.parse_args()
+        lecture_name = args['lecture_name']
+        lecture_year = args['lecture_year']
+        lecture_semester = args['lecture_semester']
+        lecture_major=args['lecture_major']
+        lecture_separated= args['lecture_separated']
+        lecture_professor = args['lecture_professor']
+        attendee=args['lecture_attendee']
+        make_lecture(lecture_name,lecture_year,lecture_semester,lecture_major,lecture_separated,lecture_professor,attendee,user_info)#추후 에러코드관리
+        return{"msg" : "lecture make success"},201
+
+class React_Lecture_mod(Resource):
+    @professor_required()
+    def get(self):
+        user_list=get_all_user()
+        parser = reqparse.RequestParser()
+        parser.add_argument('lecture_no', type=int )
+        args = parser.parse_args()
+        lecture_no = args['lecture_no']
+        mod_list,attend_list=mod_lecutre_listing(lecture_no)
+        user_info=get_jwt_identity()
+        return {   "userlist":user_list,
+                    "lectureNo": lecture_no,
+                    "modlist" : mod_list,
+                    "attendlist" : attend_list,
+                    "userInfo": user_info,
+                },200
+    @professor_required()
+    def post(self):#강의수정권한관리 만든사람, 관리자
+        user_info=get_jwt_identity()
+        parser = reqparse.RequestParser()
+        parser.add_argument('lecture_no', type=int )
+        parser.add_argument('lecture_name', type=str )
+        parser.add_argument('lecture_year', type=str )
+        parser.add_argument('lecture_semester', type=str)
+        parser.add_argument('lecture_major', type=str)
+        parser.add_argument('lecture_separated', type=str)
+        parser.add_argument('lecture_professor', type=str)
+        parser.add_argument('lecture_attendee', type=str, action='append')
+        args = parser.parse_args()
+        lecture_no = args['lecture_no']
+        lecture_name = args['lecture_name']
+        lecture_year = args['lecture_year']
+        lecture_semester = args['lecture_semester']
+        lecture_major=args['lecture_major']
+        lecture_separated= args['lecture_separated']
+        lecture_professor = args['lecture_professor']
+        attendee=args['lecture_attendee']
+        if(lecture_access_check(user_info["user_no"],lecture_no) or user_info["user_perm"]==0):
+            modify_lecture(lecture_no,lecture_name,lecture_year,lecture_semester,lecture_major,lecture_separated,lecture_professor,attendee,user_info)#추후 에러코드관리
+            return{"msg" : "lecture modify success",
+                   "lecturemodifySuccess" : 1
+                   },200
+        else:
+            return{"msg": "access denied", "lecturemodifySuccess" : 0}

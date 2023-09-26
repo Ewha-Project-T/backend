2372717b89245c6b1fd546127812b8159a1d75c3
강효은 <102407928+hyooz-Kang@users.noreply.github.com>
2023-09-26 15:17:36 +0900

    json output ver

--- kr_stt_work.py
+++ kr_stt_work.py
@@ -1,4 +1,4 @@
-#새로운 버전_12023.09 (gpt 사용 버전)
+#new_(gpt 사용+json output)
 
 from pydub import AudioSegment, silence
 from nltk.tokenize import sent_tokenize
@@ -11,6 +11,7 @@ import json
 import time
 import uuid
 import openai #openai 라이브러리 install 필요
+import ast 
 
 class KorStt:
     def process_stt_result(self,stt):
@@ -24,9 +25,7 @@ class KorStt:
         result = response.choices[0].message.content
         return result
 
-    def basic_indexing(self,filename):
-        filepath = f"{os.environ['UPLOAD_PATH']}/{filename}.wav"
-        myaudio = AudioSegment.from_file(filepath)
+    def indexing(self, myaudio):
         dBFS = myaudio.dBFS
         sound = silence.detect_nonsilent(
             myaudio, min_silence_len=1000, silence_thresh=dBFS - 16, seek_step=100)  # 1초 이상의 silence
@@ -40,135 +39,146 @@ class KorStt:
             endidx.append(sound[i][1] + 200)
             if i < len(sound) - 1:
                 silenceidx.append(sound[i + 1][0] - sound[i][1])
-        return length, sound, startidx, endidx, silenceidx, myaudio
+        return sound, startidx, endidx, silenceidx
 
-    def basic_do_stt(self,length,res, sound, startidx, endidx, silenceidx):
+
+    def do_stt(self, sound, myaudio, startidx, endidx, silenceidx):
+        url = os.environ["STT_URL"]
+
+        headers = {
+        'Content-type': 'audio/wav;codec="audio/pcm";',
+        'Ocp-Apim-Subscription-Key': os.environ["STT_KEY"],
+        #   'Authorization': Bearer ACCESS_TOKEN'
+        }
+
         flag = True
         text = ''
         delay_result = 0
         pause_result = 0
-        pause_idx=[]
-        values = res["values"]
-        tmp_textFile= ['' for i in range(len(values))]
-        for i in range(len(values)):
-            tmp_seq=values[i]["name"]
-            tmp_seq = re.findall("-?\d+", tmp_seq)
-            if not tmp_seq:#report.json 처리
-                continue
-            tmp_seq=int(tmp_seq[0])
-            kind = values[i]["kind"]
-            if kind != "Transcription":
-                continue
+        pause_idx = []
+        start_idx = []
+        end_idx = []
+        pause_final = ''
+        for i in range(len(sound)):
+            myaudio[startidx[i]:endidx[i]].export("temp.wav", format="wav")
+            # To recognize speech from an audio file, use `filename` instead of `use_default_microphone`:
+            with open('temp.wav', 'rb') as payload:
+                response = requests.request(
+                    "POST", url, headers=headers, data=payload)
+                if response.status_code != 200:
+                    raise RuntimeError("API server does not response correctly")
+                dic = json.loads(response.text)
+                if dic.get("RecognitionStatus") == "Success":
+                    stt = dic.get("DisplayText")
+                    text = text + stt
+                    if len(stt)>0:
+                        start_idx.append(startidx[i])
+                        end_idx.append(endidx[i])
+                    sentences = sent_tokenize(stt)
+                    for sentence in sentences:
+                        pause_final += sentence
+                        pause_final += ' '
 
-            recog = requests.get(values[i]["links"]["contentUrl"]).json()
-            if(not recog["combinedRecognizedPhrases"]):
-                continue
-            stt = self.process_stt_result(recog["combinedRecognizedPhrases"][0]["lexical"])
-            text = stt
-            sentences = sent_tokenize(stt)
-            for sentence in sentences:
-                if sentence.endswith('.'):
-                    flag = True
+                    if i < len(sound) - 1:
+                        pause_final += "(pause)" #"(pause: " + str(silenceidx[i]/1000)+"sec) "
+                        text = text+' '
+                        pause_result += silenceidx[i]
+                        pause_idx.append(silenceidx[i])
                 else:
-                    flag = False
-            if i < len(sound) - 1:
-                if flag == False:
-                    print("(pause: " + str(silenceidx[i])+"sec)")  # 침묵
-                    text = text+'\n'
-                    pause_idx.append(silenceidx[i])
-                else:
-                    # 통역 개시 지연구간
-                    print("(delay: " + str(silenceidx[i]) + "sec)")
-                    text = text+'\n'
-                    delay_result += silenceidx[i]
-            tmp_textFile[tmp_seq] = text
+                    continue
+            os.remove("temp.wav")
+        return pause_final, text, pause_result, delay_result, pause_idx, start_idx, end_idx
 
-        tmp_text=''.join(tmp_textFile)                
-        return tmp_text, pause_result, delay_result, pause_idx, startidx, endidx
-
-    def basic_annotation_stt(self,result,stt,pause_idx):
+    def basic_annotation_stt(self, result,stt,pause_final,pause_idx):
         p = re.compile('(\w\(filler\)|\w+\s\w+\(cancellation\))')
-        fidx = []
+        aidx = []
+
         while (True):
             f = p.search(stt)
             if f == None:
                 break
-            fidx.append([f.start(), f.end()])
+            aidx.append([f.start(), f.end()])
             stt = re.sub("(\(filler\)|\(cancellation\))", "", stt, 1)
 
-        for i in range(len(fidx)):
-            if stt[fidx[i][0]] == '음' or stt[fidx[i][0]] == '그' or stt[fidx[i][0]] == '어'or stt[fidx[i][0]] == '아':
-                result['annotations'].append({'start': fidx[i][0], 'end': fidx[i][0] + 1, 'type': 'FILLER'})
-            else:
-                result['annotations'].append({'start': fidx[i][0], 'end': fidx[i][1] - 14, 'type': 'CANCELLATION'})
+        for i in range(len(aidx)):
+            if stt[aidx[i][0]] == '음' or stt[aidx[i][0]] == '그' or stt[aidx[i][0]] == '어'or stt[aidx[i][0]] == '아':
+                result['annotations'].append({'start': aidx[i][0], 'end': aidx[i][0] + 1, 'type': 'FILLER'})
+            else :
+                result['annotations'].append({'start': aidx[i][0], 'end': aidx[i][1] - 14, 'type': 'CANCELLATION'})
 
-        pidx = [m.start(0) + 1 for m in re.finditer('[^\.^\n]\n', stt)]
+        p = [m.start(0) for m in re.finditer('\(pause\)', pause_final)]
+
+        pidx = []
+        for i in range(len(p)):
+          tmp = p[i] - i*7
+          pidx.append(tmp)
+
         for i in range(len(pidx)):  # pause, delay 구분 없이 pause 로 통일
-            result['annotations'].append({'start': pidx[i], 'end': pidx[i] + 1, 'type': 'PAUSE', 'duration': pause_idx[i]})
+            result['annotations'].append({'start': pidx[i]-1, 'end': pidx[i], 'type': 'PAUSE', 'duration': pause_idx[i]})
 
         if stt.endswith("\n"):
             stt = stt[:-1]
-        result['textFile']=stt   
+        result['textFile']=stt
+
         return result
 
-    def request_api(self,length,myaudio,startidx,endidx):
-        domain = os.getenv("DOMAIN", "https://edu-trans.ewha.ac.kr:8443")
-
-        files = []
-        local_file = []
-        
-        for i in range(length):
-            filetmp = uuid.uuid4()
-            filepath = f"{os.environ['UPLOAD_PATH']}/{filetmp}.wav"
-            myaudio[startidx[i]:endidx[i]].export(filepath, format="wav")
-            files += [ domain + "/" + filepath ]
-            local_file += [ filepath ]
-
-        if not len(files) > 0:  
-            return None  
-        res={'values':[]}
-
-        webhook_res = requests.post(
-            os.environ["STT_URL"],
-            headers={
-                "Content-Type": "application/json",
-                "Ocp-Apim-Subscription-Key": os.environ["STT_KEY"]
-            },
-            json={
-                "contentUrls": files,
-                "properties": {
-                    "diarizationEnabled": False,
-                    "wordLevelTimestampsEnabled": True,
-                    "punctuationMode": "DictatedAndAutomatic",
-                    "profanityFilterMode": "Masked"
-                },
-                "locale": "ko-KR",
-                "displayName": "Transcription of file using default model for en-US"
-            }
-        ).json()
-        url = webhook_res["links"]["files"]
-        while len(res["values"]) == 0:
-            res = requests.get(
-                url,
-                headers={ "Ocp-Apim-Subscription-Key": os.environ['STT_KEY'] }
-            ).json()
-
-            time.sleep(1)
-        for file in local_file:
-            os.unlink(file)          
-        return res
     
-    def execute(self,filename):
+    def stt_json(self, myaudio):
+        sound, startidx, endidx, silenceidx = self.indexing(myaudio)
+        pause_final, stt_t, pause_result, delay_result, pause_idx, start_idx, end_idx = self.do_stt(
+            sound, myaudio, startidx, endidx, silenceidx)
+
+        stt_t = stt_t.replace('\n', '') #stt_t
+        stt = self.process_stt_result(stt_t)
+
         result = {'textFile': '', 'timestamps': [], 'annotations': []}
-        length,sound,startidx,endidx,silenceidx,myaudio=self.basic_indexing(filename)
-        res=self.request_api(length,myaudio,startidx,endidx)
-        if res==None:  
-            raise Exception("INVALID-FILE")        
+        for i in range(len(start_idx)):
+            result['timestamps'].append({'start': start_idx[i], 'end': end_idx[i]})
 
-        for i in range(length):
-            result['timestamps'].append({'start': startidx[i], 'end': endidx[i]})
+        result = self.basic_annotation_stt(result,stt,pause_final,pause_idx)
 
-        stt_text, pause_result, delay_result, pause_idx, startidx, endidx=self.basic_do_stt(length,res,sound,startidx,endidx,silenceidx)
-        result=self.basic_annotation_stt(result,stt_text,pause_idx)
-        return result,sound,startidx,endidx,silenceidx
+        result['textFile'] = stt_t
+
+        return result
+
+    
+    def parse_data(self, stt_result,stt_feedback):
+        cnt=1
+        text=""
+        denotations="["
+        attributes="["
+        text=text+stt_result['textFile']+"\n"
+        for j in range(len(stt_feedback)):
+            denotations +='{ "id": "T'+str(cnt)+'", "span": { "begin": '+str(stt_feedback[j]['start'])+', "end": '+str(stt_feedback[j]['end'])+' }, "obj": "'+str(stt_feedback[j]['type'])+'" },'
+            attributes +='{ "id": "A'+str(cnt)+'", "subj": "T'+str(cnt)+'", "pred": "Unsure", "obj": True },'
+            cnt+=1
+        denotations=denotations[:-1]
+        attributes=attributes[:-1]
+        denotations+="]"
+        attributes+="]"
+        return text,denotations,attributes
+
+    
+    def make_json(self, text,denotations,attributes):
+        data = {
+            "text": text,
+            "denotations": ast.literal_eval(denotations) if type(denotations) == str else denotations ,
+            "attributes": ast.literal_eval(attributes) if type(attributes) == str else attributes,
+        }
+        
+        return data
+
+
+    # 마지막 실행 함수
+    def last_output(self, filename):
+        filepath = f"{os.environ['UPLOAD_PATH']}/{filename}.mp3" #파일형태 변경
+        myaudio = AudioSegment.from_file(filepath)
+        stt_result = self.stt_json(myaudio)
+        stt_feedback = stt_result['annotations']
+        text,denotations,attributes = self.parse_data(stt_result,stt_feedback)
+        data = self.make_json(text,denotations,attributes)
+
+        return json.dumps(data, indent=4,ensure_ascii=False)
+
+
     #한국어버전 끝

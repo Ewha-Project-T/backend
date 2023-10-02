28b6a3505e4b7bd23c725e3fd87e76fbeced16a1
marinesnow34 <m4rinesnow34@gmail.com>
2023-10-03 01:00:30 +0900

    Confix: 경로 변경

--- server/apis/__init__.py
+++ server/apis/__init__.py
@@ -55,7 +55,7 @@ def load_api(api_module):
     api_module.add_resource(React_Prob_detail, '/api/prob/detail', endpoint='react_prob_detail') #
     api_module.add_resource(React_Prob_student, '/api/prob/student', endpoint='react_prob_student') #
     api_module.add_resource(React_Porb_professor, '/api/prob/professor', endpoint='react_prob_professor') #
-    api_module.add_resource(prob_upload_file, '/prob_upload_file', endpoint='prob_upload_file') #
+    api_module.add_resource(prob_upload_file, '/api/prob_upload_file', endpoint='prob_upload_file') #
     api_module.add_resource(React_Prob_record, '/api/prob/record', endpoint='react_Prob_record') #
     api_module.add_resource(React_Prob_end_submission, '/api/prob/end', endpoint='React_Prob_end_submission') #
 
@@ -64,7 +64,7 @@ def load_api(api_module):
     api_module.add_resource(Login2,'/api/user/login',endpoint='login2') # GET POST PUT PATCH DELETE
     api_module.add_resource(React_Lecture,'/api/lecture/list',endpoint='react_lecture_list')
     api_module.add_resource(Logout2,'/api/user/logout',endpoint='logout2') # GET POST PUT PATCH DELETE
-    api_module.add_resource(prob_upload, '/prob_upload', endpoint='prob_upload')
+    api_module.add_resource(prob_upload, '/api/prob_upload', endpoint='prob_upload')
     api_module.add_resource(Stt, '/stt',endpoint='stt')
     #추후 삭제 예정
     #api_module.add_resource(React_Prob_mod, '/api/prob/modify', endpoint='react_prob_mod')

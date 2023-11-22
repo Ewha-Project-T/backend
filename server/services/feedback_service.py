4372a20dd3c0ad79de3f7b706953b800d44f9a77
marinesnow34 <m4rinesnow34@gmail.com>
2023-11-22 23:31:44 +0900

    Feat: 오류 처리

--- server/services/feedback_service.py
+++ server/services/feedback_service.py
@@ -120,6 +120,8 @@ def get_feedback_info(as_no: int, student_no: int, user_no: int):
         stt_job = SttJob.query.filter_by(stt_no=st.stt_no).first()
         if stt_job is None:
             return {"message": "교수님의 음원 STT 작업 진행중입니다.", "isSuccess": False}
+        if stt_job.stt_result is None:
+            return {"message": "STT 오류 STT를 다시 진행해 주세요.", "isSuccess": False}
         result = json.loads(stt_job.stt_result)
         text += result["text"]
         # denotations_json += result["denotations"]

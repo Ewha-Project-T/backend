0af10b65a937b514e96fda8d88ec55daa4b7845f
Joowon Lim <m4rinesnow34@gmail.com>
2024-01-03 21:01:50 +0900

    Feat: 그래프 갱신 추가

--- server/apis/react_feedback.py
+++ server/apis/react_feedback.py
@@ -3,7 +3,7 @@ from flask import jsonify, Flask, request, make_response
 from flask_restful import reqparse, Resource
 from flask_jwt_extended import jwt_required, get_jwt_identity
 
-from ..services.feedback_service import get_all_graphs, get_feedback_info, get_feedback_review, get_json_textae, get_my_graphs, get_zip_url, put_json_textae, save_feedback_review
+from ..services.feedback_service import get_all_graphs, get_feedback_info, get_feedback_review, get_json_textae, get_my_graphs, get_zip_url, put_json_textae, save_feedback_review, update_graph
 
 class Feedback_textae(Resource):
     parser = reqparse.RequestParser()
@@ -125,6 +125,17 @@ class Feedback_student_graph(Resource):
         user_info = get_jwt_identity()
         res = get_my_graphs(args['as_no'], user_info['user_no'])
         return jsonify(res)
+
+class Feedback_graph_update(Resource):
+    @jwt_required()
+    def get(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('as_no', type=int, required=True)
+        parser.add_argument('student_no', type=int, required=True)
+        args = parser.parse_args()
+        user_info = get_jwt_identity()
+        res = update_graph(as_no=args['as_no'], user_no=args['student_no'])
+        return jsonify({"isSuccess":True})
 
 class assignment_zip_down(Resource): # 과제 압축파일 다운로드 개발 후 삭제
     def get(self):

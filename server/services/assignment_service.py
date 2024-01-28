0b5289adf0f22902604a8b9379a2d09be692b8bb
Joowon Lim <m4rinesnow34@gmail.com>
2024-01-28 15:57:49 +0900

    Fix: 과제 이름 삭제

--- server/services/assignment_service.py
+++ server/services/assignment_service.py
@@ -627,7 +627,7 @@ def get_prob_submit_list(as_no,lecture_no):
         if check.review!=None:
             tmp["status"] = "작성 완료"
             tmp["test"] = 3
-        trans_file = make_trans_file(as_check, as_check.assignment.as_name, user.name)
+        trans_file = make_trans_file(as_check, user.name)
         if trans_file:
             tmp["file"] = trans_file
         submit_list.append(tmp)
@@ -901,14 +901,14 @@ def assignment_chance(as_no:int, user_no:int,student_no:int):
 
     return {"message" : "과제 기회 추가 완료"}
 
-def make_trans_file(assignment_check:Assignment_check, assignment_name:str, student_name:str):
+def make_trans_file(assignment_check:Assignment_check, student_name:str):
     if assignment_check is None:
         return None
     if assignment_check.user_trans_result is None:
         return None
 
     # Create a unique filename using uuid
-    filetmp = assignment_check.check_no+ "_" + assignment_name + "_" + student_name
+    filetmp = assignment_check.check_no+ "_" + student_name
     filepath = f"{os.environ['UPLOAD_PATH']}/{filetmp}.txt"
 
     # Write the translation result to the file
@@ -917,3 +917,6 @@ def make_trans_file(assignment_check:Assignment_check, assignment_name:str, stud
     
     # Return the path of the created file
     return filepath
+
+def make_student_audio_zip(assignment:Assignment):
+    return

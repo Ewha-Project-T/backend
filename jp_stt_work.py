4af0af20ff35eb440ced44bfb1359cae6df1f502
KonWooKim <kken1204@gmail.com>
2024-04-18 00:12:48 +0900

    Change model and add seed

--- jp_stt_work.py
+++ jp_stt_work.py
@@ -31,7 +31,8 @@ class JpStt:
         )
 
         response = client.chat.completions.create(
-            model="gpt-4",
+            model="gpt-4-0125-preview",
+            seed=42,
             messages=[{"role": "user", "content": prompt_message}],
             temperature=0,
             top_p=1,
